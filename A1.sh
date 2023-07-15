3dde296e0a11566be2550c0f8b0ef27cbe65efee
mike90s15 <82988362+mike90s15@users.noreply.github.com>
2023-07-15 05:39:16 +0000

    Update A1.sh

--- A1.sh
+++ A1.sh
@@ -1,9 +1,18 @@
-xdg-open https://www.instagram.com/mike90s15/
-sleep 5
-xdg-open https://chat.whatsapp.com/HCavGIY9pR35BRZjoLoFGP
+#!/usr/bin/env bash
+# by: @Mike90s15 (Mike Edwards)
+# 07/14/2023
+# version: 0.0.1
+printf "\ec\e[1;36m
+ Criador: Mike90s15 (Mike Edwards)
+ Data: 14/07/2023
+ Versão: 0.0.1
+ \e[m\n"
+ xdg-open https://instagram.com/mike90s15 &>/dev/null && sleep 5
+ xdg-open https://t.me/Mike_Edwards &>/dev/null && sleep 5
+ printf "\e[1;32m Incializando o programa...\n"
 pkg="apt-get"
 [[ "${HOME}" == "/data/data/com.termux/files/home" ]] && pkg="pkg"
-printf " Instalando as dependências!"
+printf "\e[1;32m Instalando as dependências!"
 ${pkg} install clang -y &> /dev/null
 [[ -z $(command -v clang ) ]] && echo " Erro" && exit 1
 gcc main.c -o main
